d633bd3b8358092bef03203cdf8e60af83981b27
Vansh Jaiswal <vanshjaiswalofficial@gmail.com>
2025-10-20 23:54:42 +0530

    Addition of two numbers

---
+++ ps1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"Enter Num1 : ";
+    cin>>num1;
+    cout<<"Enter Num2 : ";
+    cin>>num2;
+
+    cout<<"Sum of two number is "<<num1+num2;
+
+    return 0;
+
+}
