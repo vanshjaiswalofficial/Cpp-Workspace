fc6b8d42718d9a85319a6cc4d886363dc69889cc
Vansh Jaiswal <vanshjaiswalofficial@gmail.com>
2025-10-22 23:58:46 +0530

    Find Area of a Circle

---
+++ ps3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    float area,r;
+
+    cout<<"\nEnter the value of radius (r) : ";
+    cin>>r;
+
+    area=3.14*r*r;
+
+    cout<<"\nArea of circle is : "<<area;
+
+    return 0;
+}
