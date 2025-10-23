d3e8bbe351d433d5a8d20436fc3667706d6a0d26
Vansh Jaiswal <vanshjaiswalofficial@gmail.com>
2025-10-23 19:25:06 +0530

    Compute average of 3 numbers

---
+++ ps8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num1,num2,num3;
+
+    cout<<"\nEnter Number 1 : ";
+    cin>>num1;
+
+    cout<<"Enter Number 2 : ";
+    cin>>num2;
+    
+    cout<<"Enter Number 3 : ";
+    cin>>num3;
+
+    cout<<"\nAvg of 3 number : "<<(num1+num2+num3)/3<<endl;
+    cout<<endl;
+
+    return 0;
+}
